3332aa07514cc84952f1bd80d58fdc0f9d02dcbe
oluwafemi <oluwafemiogunleye21@gmail.com>
2022-05-29 16:25:10 +0100

    add jquery

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $("p").click(function() {
+
+        alert("This is a paragraph.");
+    
+      });
+    
+      $("img").click(function(){
+          alert("this is an image.");
+      });
+    });
